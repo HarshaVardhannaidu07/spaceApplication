c881e682b901c1379f3496b6ae5b2875daef133e
HarshaVardhannaidu07 <96299815+HarshaVardhannaidu07@users.noreply.github.com>
2025-05-15 11:03:57 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,11 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Application of Mars',
+  description: 'Form to Mars',
+  icons: {
+    icon: "/mars.png",
+  },
 }
 
 export default function RootLayout({
